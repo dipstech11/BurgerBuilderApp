0608ff55b80b21a514fb37a87b8da725ddebdc47
dipsdeepak11 <deepak.yadav@xoriant.com>
2019-05-01 22:56:39 +0530

    Able to Display burger, ingredient(dynamically) and build control display(No logic yet)

--- burgerbuilder/src/components/Burger/Burger.js
+++ burgerbuilder/src/components/Burger/Burger.js
@@ -5,13 +5,25 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 
 const burger = (props) => {
+  let transformIngredient = Object.keys(props.ingredients)    //[salad,cheese]
+  .map(igkey => {                                               //igkey = salad, chees
+//...Array(props.ingredients[igkey]) =this will return a array of length of key with null value.
+    return [...Array(props.ingredients[igkey])].map((_, i) =>{   //finding the value, _ = value
+    return <BurgerIngredient key={igkey+i} type={igkey} />      //like 2 or 3, i = len(value)
+    })
+  }).reduce((arr, el) =>{
+    return arr.concat(el)
+  },[])
+
+  if (transformIngredient.length === 0) {
+      transformIngredient = <p>Please start adding ingredient</p>
+  }
+
   return (
     <div className={classes.Burger}>
         <BurgerIngredient type="bread-top"/>
-        <BurgerIngredient type="cheese"/>
-        <BurgerIngredient type="meat"/>
+        {transformIngredient}
         <BurgerIngredient type="bread-bottom"/>
-
     </div>
   );
 }
--- burgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
 import Auxx from '../../hoc/auxx'
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
 
 class BurgerBuilder extends Component{
+  state = {
+    ingredients:{
+      salad:0,
+      bacan:0,
+      cheese:0,
+      meat:0
+    }
+  }
+
   render()
     {
       return (
           <Auxx>
-          <Burger />
-          <div> Build Control </div>
+          <Burger ingredients = {this.state.ingredients}/>
+          <BuildControls />
           </Auxx>
       );
     }
