583459b10b806b38700278f538656adfd9b57bce
dipsdeepak11 <deepak.yadav@xoriant.com>
2019-05-08 20:00:07 +0530

    Burger Builder Basic Completed

--- burgerbuilder/src/components/Navigation/Toolbar/Toolbar.js
+++ burgerbuilder/src/components/Navigation/Toolbar/Toolbar.js
@@ -6,8 +6,9 @@ import NavigationItems from '../NavigationItems/NavigationItems'
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-      <div>MENU </div>
-      <div>LOGO</div>
+      <div className={classes.Logo}>{
+      // <Logo
+      }</div>
       <nav> <NavigationItems /> </nav>
     </header>
 
